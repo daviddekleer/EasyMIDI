32a4f7c6d11cd08642f9f49678bb72517d15bb91
daviddekleer <ddekleer@kpnplanet.nl>
2017-10-04 16:13:05 +0200

    Added some info to EasyMIDI source file.

--- EasyMIDI/EasyMIDI.py
+++ EasyMIDI/EasyMIDI.py
@@ -1,3 +1,38 @@
+#
+# Name:        EasyMIDI.py
+# Purpose:     Easy MIDI manipulation for algorithmic composition
+#
+# Author:      David de Kleer <daviddekleer) at )(*outlook . )com>
+#
+# Created:     04-07-2015
+# Copyright:   (c) 2015-2017 David de Kleer
+# License:     Please see LICENSE.txt for the terms under which this
+#              software is distributed.
+#
+# A simple, easy to use algorithmic composition MIDI creator for Python, based on midiutil.
+#
+# Creating a MIDI file can be as simple as this::
+#
+#    from EasyMIDI import EasyMIDI,Track,Note,Chord,RomanChord
+#    from random import choice
+#
+#    easyMIDI = EasyMIDI()
+#    track1 = Track("acoustic grand pino")  # oops
+#
+#    c = Note('C', octave = 5, duration = 1/4, volume = 100)
+#    e = Note('E', 5)
+#    g = Note('G', 5)
+#    chord = Chord([c,e,g])  # a chord of notes C, E and G
+#    track1.addNotes([c, e, g, chord])
+#
+#    # roman numeral chord, first inversion (defaults to key of C)
+#    track1.addNotes(RomanChord('I*', octave = 5, duration = 1))
+#
+#    easyMIDI.addTrack(track1)
+#    easyMIDI.writeMIDI("output.mid")
+#
+
+
 from copy import deepcopy
 from difflib import SequenceMatcher
 import os
